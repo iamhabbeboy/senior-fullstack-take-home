2d6bba16f053523e551e7a9fb68a69c59ce01eae
iamhabbeboy <iamhabbeboy@gmail.com>
2021-06-11 06:21:44 +0100

    refactor: update schema

--- php-soapservice/config/schema.sql
+++ php-soapservice/config/schema.sql
@@ -1,89 +1,92 @@
 CREATE TABLE `companies` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `created_at` timestamp,
-  `updated_at` timestamp,
-  `name` varchar(255),
-  `email` varchar(255),
-  `logo_url` varchar(255),
-  `address` varchar(255),
-  `country` varchar(255),
-  `tax_rate` decimal(5,2)
-);
-
-
-CREATE TABLE `users` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `created_at` timestamp,
-  `updated_at` timestamp,
-  `company_id` int DEFAULT NULL,
-  `first_name` varchar(100),
-  `last_name` varchar(100),
-  `avatar_url` varchar(255),
-  `email` varchar(100),
-  `role` varchar(100),
-  `hourly_rate` float,
-  `address` text,
-  `phone` varchar(30)
+  `id` int NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `logo_url` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `tax_rate` decimal(5,2) DEFAULT NULL
 );
 
 CREATE TABLE `holidays` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `created_at` timestamp,
-  `updated_at` timestamp,
-  `user_id` int,
-  `start_date` timestamp,
-  `end_date` timestamp,
-  `status` ENUM ('approve', 'decline', 'pending')DEFAULT 'pending'
-);
-
-CREATE TABLE `service_categories` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `created_at` timestamp,
-  `updated_at` timestamp,
-  `name` varchar(100)
+  `id` int NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  `start_date` timestamp NULL DEFAULT NULL,
+  `end_date` timestamp NULL DEFAULT NULL,
+  `status` enum('approve','decline','pending') DEFAULT 'pending'
 );
 
 CREATE TABLE `services` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `created_at` timestamp,
-  `updated_at` timestamp,
-  `service_category_id` int,
-  `company_id` int,
-  `name` varchar(255)
+  `id` int NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `service_category_id` int DEFAULT NULL,
+  `company_id` int DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL
+);
+
+CREATE TABLE `service_categories` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL
 );
 
 CREATE TABLE `service_rates` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `created_at` timestamp,
-  `updated_at` timestamp,
-  `service_id` int,
-  `unit` float,
-  `amount` float, 
-  `duration` float,
-  `supply_markup` float,
-  `overhead_markup` float,
-  `misc_markup` float
+  `id` int NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `service_id` int DEFAULT NULL,
+  `company_id` int NOT NULL,
+  `unit` float DEFAULT NULL,
+  `amount` float DEFAULT NULL,
+  `duration` varchar(50) DEFAULT NULL,
+  `supply_markup` float DEFAULT NULL,
+  `overhead_markup` float DEFAULT NULL,
+  `misc_markup` float DEFAULT NULL,
+  `service_request_id` int NOT NULL
 );
 
 CREATE TABLE `service_request` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `created_at` timestamp,
-  `updated_at` timestamp,
-  `service_category_id` int,
-  `company_id` int,
-  `name` varchar(255)
+  `id` int NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `issue_id` int NOT NULL,
+  `user_id` int DEFAULT NULL,
+  `company_id` int DEFAULT NULL,
+  `proposed_start_date` datetime DEFAULT NULL,
+  `proposed_end_date` datetime NOT NULL,
+  `actual_start_date` datetime NOT NULL,
+  `actual_end_date` datetime DEFAULT NULL,
+  `title` varchar(200) NOT NULL,
+  `status` varchar(100) NOT NULL,
+  `adjustment` text NOT NULL
 );
 
-CREATE TABLE `work_order` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `created_at` timestamp,
-  `updated_at` timestamp,
-  `service_request_id` int,
-  `employee_id` int,
-  `status`  ENUM ('approve', 'decline', 'pending', 'started', 'completed', 'paid')DEFAULT 'pending'
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `company_id` int DEFAULT NULL,
+  `first_name` varchar(100) DEFAULT NULL,
+  `last_name` varchar(100) DEFAULT NULL,
+  `avatar_url` varchar(255) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `role` varchar(100) DEFAULT NULL,
+  `hourly_rate` float DEFAULT NULL,
+  `address` text,
+  `phone` varchar(30) DEFAULT NULL
 );
 
-
-
-
-
+CREATE TABLE `work_orders` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `service_request_id` int DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  `status` enum('approve','decline','pending','started','completed','paid') DEFAULT 'pending'
+);
